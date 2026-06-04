ad065f2398ed363ba2d0eaaaffc2dbdf795b7a1c
shivampoojanyadav <yshivampoojan@gmail.com>
2026-06-04 10:51:35 +0530

    fix all the error

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 
 from .utils import get_best_answer
 
-
 def home(request):
     return render(request, "chatbot/index.html")
 
-
+@csrf_exempt
 def get_response(request):
 
     if request.method == "POST":
